989762bb3eb5318b63a23e632f1a6e83cbfe9216
Tarek Ashraf <tarekshalaby2015@gmail.com>
2024-09-18 02:05:28 +0300

    adding files

--- app/server.py
+++ app/server.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI, HTTPException, Response, Request
-from .openai_rag  import conversational_rag_chain
+from .llm_chain import conversational_rag_chain
 from pydantic import BaseModel
 import uuid
 
